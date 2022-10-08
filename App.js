0136589ce94106d68b2484aef74116a7404b2c22
reindra-ubuntu <reindrairawan@gmail.com>
2022-10-09 00:08:29 +0700

    add home screen layer on navigator

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import * as React from 'react';
+import {View, Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-export default function App(){
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function DashboardScreen(){
+return (
+  <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>Dashboard Screen</Text>
+  </View>
+);
+}
   return (
     <NavigationContainer>{ }</NavigationContainer>
   )
