c6b863c38d691f616f697912b28a4633f56006d9
Reindra12 <46821392+Reindra12@users.noreply.github.com>
2022-10-09 21:15:40 +0700

    02 setup navigation (#3)
    * Setup React Native
    
    * Wrapping  app in NavigationContainer
    
    * import libraries
    
    * added oncreate method in mainactivity
    
    * changes to the mainactivity class
    
    * add home screen layer on navigator
    
    * add Detail screen layer on navigator
    
    * NavigationContainer -> to set screen(Configuring)
    
    * Specifying options​
    
    * Passing additional props​
    
    * Navigating to a Detail screen
    
    * Navigate to a route multiple times
    
    * validation navagator (Going back)
    
    * customization view
    
    * Passing data

--- App.js
+++ App.js
@@ -1,117 +1,177 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Stack = createNativeStackNavigator();
+const Separator = () => (
+  <View style={style.separator}/>
+);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+
+function DashboardScreen({ navigation }){
+return (
+  <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text style={style.title}>
+      kali ini belajar bagaimana menggunakan navigator .
+       klik tombol dibawah untuk pindah ke halaman detail produk.
+    </Text>
+    <Button 
+    title='Go to Details Product'
+    onPress={() => navigation.navigate('Details',{itemId:86,
+    otherParam: 'Data ini diperoleh dari dashboard layer',})}
+    />
+  </View>
+);
+}
+
+function DetailsProduct({route, navigation}){
+
+  const {itemId, otherParam} = route.params;
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+  <SafeAreaView style={style.container}>
+  <View>
+    
+    <Text style={style.title}>
+      Klik menu dibawah untuk melakukan pindah layer / class ke Detail Produk.
+    </Text>
+
+    <Text style= {style.title}>
+      itemId: {JSON.stringify(itemId)}
+    </Text>
+
+    <Text style = {style.title}>
+      otherParam: {JSON.stringify(otherParam)}
+    </Text>
+
+    <Button
+      title="Go to Details... again"
+      onPress={() => navigation.push('Details', {
+        itemId:Math.floor(Math.random()*100),
+      })
+    }
+    />
+  </View>
+  <Separator />
+  <View>
+    <Text style={style.title}>
+    Klik menu dibawah ini untuk melakukan pindah layer / class ke Halaman Utama.
+    </Text>
+    <Button
+      title="Go to Dashboard"
+      color="#f194ff"
+      onPress={() => navigation.navigate('Home')}
+    />
+  </View>
+
+  <Separator />
+  <View>
+    <Text style={style.title}>
+    Go Back.
+    </Text>
+    <Button
+      title="Go Back"
+      color="#f194ff"
+      onPress={() => navigation.navigate('Home')}
+    />
+  </View>
+  <Separator />
+
+
+  <View>
+    <Text style={style.title}>
+    Klik menu dibawah ini untuk melakukan pindah layer / class ke Halaman Utama.
+    </Text>
+    <Button
+      title="Go back to first screen in stack"
+      color="#f194ff"
+      onPress={() => navigation.popToTop()}
+    />
+  </View>
+  <Separator />
+
+  <View>
+    <Text style={style.title}>
+     Disable 
+    </Text>
+    <Button
+      title="Saya tidak bisa di pencet"
+      disabled
+      onPress={() => Alert.alert('Cannot press this one')}
+    />
+  </View>
+  <Separator />
+
+
+  <View>
+    <Text style={style.title}>
+      This layout strategy lets the title define the width of the button.
+    </Text>
+   
+    <View style={style.fixToText}>
+      <Button
+        title="Left button"
+        onPress={() => Alert.alert('Left button pressed')}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+      <Button
+        title="Right button"
+        onPress={() => Alert.alert('Right button pressed')}
+       
+      />
+    </View>
+  </View>
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+</SafeAreaView>
+  )
+}
+
+
+function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen 
+        name = "Home">
+          {(props) => <DashboardScreen {...props} extraData = {dummyData}/>}
+          </Stack.Screen> 
+        {/* component={DashboardScreen} */}
+        {/* options={{title: 'Dashboard'}}/> */}
+        <Stack.Screen name = "Details" component={DetailsProduct}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function dummyData(){
+  <Text> Passing additional props​ </Text>
+}
+
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 20
+  }, fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
+
 
 export default App;
